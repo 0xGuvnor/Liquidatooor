2fe5d576d08057e16b2aadf3e6f8686584e00396
0xpaprhnds <yokeyeong@me.com>
2022-12-25 02:20:13 +0800

    Update accounts.ts

--- tasks/accounts.ts
+++ tasks/accounts.ts
@@ -1,12 +1,11 @@
-import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment, TaskArguments } from "hardhat/types";
+import { task } from "hardhat/config";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { ethers } from "hardhat";
 
 task("accounts", "Prints the list of accounts").setAction(
     async (taskArgs: TaskArguments, hre: HardhatRuntimeEnvironment): Promise<void> => {
         const accounts: SignerWithAddress[] = await hre.ethers.getSigners();
-        const provider = ethers.provider;
+        const provider = hre.ethers.provider;
         let id = 0;
 
         for (const account of accounts) {
@@ -14,7 +13,7 @@ task("accounts", "Prints the list of accounts").setAction(
             console.log(
                 `[${id < 10 ? "0" + id.toString() : id}] ${
                     account.address
-                } -- ${ethers.utils.formatEther(balance)} ETH`
+                } -- ${hre.ethers.utils.formatEther(balance)} ETH`
             );
             id++;
         }
